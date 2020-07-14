df99889529a9e6224cda74d5a1c4d3b7237c6bc5
Nikita <almostroll@yandex.ru>
2020-07-14 22:01:17 +0300

    Some sketches

--- app/src/main/java/com/rtuitlab/geohelper/MainActivity.kt
+++ app/src/main/java/com/rtuitlab/geohelper/MainActivity.kt
@@ -5,17 +5,38 @@ import android.content.pm.PackageManager
 import android.hardware.SensorManager
 import android.location.Location
 import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.ar.core.Pose
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.Scene
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ViewRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.view_place_label.view.*
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+
 
 class MainActivity : AppCompatActivity() {
 
 	companion object {
 		const val LOG_TAG = "GeoHelperLogs"
+	}
+
+	private val arFragment by lazy {
+		supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
+	}
+
+	private val scene: Scene by lazy {
+		arFragment.arSceneView.scene
 	}
 
 	private val viewModel: MainViewModel by lazy {
@@ -28,6 +49,8 @@ class MainActivity : AppCompatActivity() {
 		)
 	}
 
+	private var anchorNode: AnchorNode? = null
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
@@ -35,8 +58,18 @@ class MainActivity : AppCompatActivity() {
 		setupLocationListener()
 
 		viewModel.locationDataLiveData.observe(this, Observer {
-			processLocationData(it)
+			Log.i("asd", "asd")
+//			processLocationData(it)
 		})
+
+		updateBtn.setOnClickListener {
+			val locationData = viewModel.locationDataLiveData.value
+			locationData?.let {
+				processLocationData(it)
+			} ?:run {
+				Toast.makeText(this@MainActivity, "Not ready!", Toast.LENGTH_SHORT).show()
+			}
+		}
 	}
 
 	override fun onResume() {
@@ -55,16 +88,73 @@ class MainActivity : AppCompatActivity() {
 			return
 		}
 		getSystemService(LocationManager::class.java)?.apply {
-			requestLocationUpdates(LocationManager.GPS_PROVIDER, 200, 0F, object : ChangeLocationListener() {
+			requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0F, object : ChangeLocationListener() {
 				override fun onLocationChanged(location: Location?) {
 					viewModel.azimuth = azimuthManager.azimuth
 					viewModel.currentLocation = location
+					Toast.makeText(
+						this@MainActivity,
+						"Location changed",
+						Toast.LENGTH_SHORT
+					).show()
 				}
 			})
 		}
 	}
 
 	private fun processLocationData(locationData: LocationData) {
+		Log.wtf(LOG_TAG, locationData.location.toString())
 
+		anchorNode?.anchor?.detach()
+		anchorNode = null
+
+		val anchor = arFragment.arSceneView.session?.createAnchor(
+			Pose(
+				floatArrayOf(0F, 0F, 0F),
+				floatArrayOf(0F, 0F, 0F, 1F)
+			)
+		)
+		anchorNode = AnchorNode(anchor).apply {
+			setParent(scene)
+		}
+
+		locationData.places.forEach {
+			val degree = it.bearingFrom(locationData.location)
+			val distant = 3.0
+
+			val y = 0F
+			val x = (distant * cos(PI * degree / 180)).toFloat()
+			val z = (-distant * sin(PI * degree / 180)).toFloat()
+			addPointByXYZ(-z, y, -x, "${it.name}\nDegree: $degree\nX,Y,Z: $x, $y, $z")
+		}
+	}
+
+	private fun addPointByXYZ(x: Float, y: Float, z: Float, name: String) {
+		ViewRenderable
+			.builder()
+			.setView(this, R.layout.view_place_label)
+			.build()
+			.thenAccept { viewRenderable ->
+				viewRenderable.view.labelTV.text = name
+
+				anchorNode?.addChild(
+					PlaceNode().apply {
+						renderable = viewRenderable
+						worldPosition = Vector3(x, y, z)
+					}
+				)
+
+//				val node = PlaceNode()
+//				node.renderable = it
+//				scene.addChild(node)
+//				node.worldPosition = Vector3(x, y, z)
+
+//				val cameraPosition = scene.camera.worldPosition
+//				val direction = Vector3.subtract(cameraPosition, node.worldPosition)
+//				val lookRotation = Quaternion.lookRotation(direction, Vector3.up())
+//				node.worldRotation = lookRotation
+
+//				scene.addChild(node)
+			}
 	}
 }
--- app/src/test/java/com/rtuitlab/geohelper/ExampleUnitTest.kt
+++ app/src/test/java/com/rtuitlab/geohelper/ExampleUnitTest.kt
@@ -3,15 +3,66 @@ package com.rtuitlab.geohelper
 import org.junit.Test
 
 import org.junit.Assert.*
+import kotlin.math.PI
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.sin
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+class BearingTest {
 	@Test
-	fun addition_isCorrect() {
-		assertEquals(4, 2 + 2)
+	fun bearing_isCorrect() {
+		val mirea = placesListTest[0]
+		val mine = Place(
+			55.928823, 39.456725, "Home"
+		)
+		assertEquals(
+			256.89,
+			bearing(mine, mirea),
+			.000001
+		)
+	}
+
+	@Test
+	fun dekart_isCorrect() {
+		val distant = 3.0
+
+		val mirea = placesListTest[0]
+		val mine = Place(
+			55.928823, 39.456725, "Home"
+		)
+		val degree = bearing(mine, mirea)
+
+		val y = 0F
+		val x = (distant * cos(PI * degree / 180)).toFloat()
+		val z = (-distant * sin(PI * degree / 180)).toFloat()
+
+		assertEquals(
+			"X: $x; Y: $y; Z: $z",
+			true, false
+		)
 	}
 }
+
+val placesListTest: List<Place> = listOf(
+	Place(55.670002, 37.480212, "РТУ МИРЭА"),
+	Place(56.126895, 40.397134, "Золотые ворота"),
+	Place(59.939817, 30.314448, "Эрмитаж"),
+	Place(58.939817, 35.314448, "Эрмитаж1"),
+	Place(57.939817, 34.314448, "Эрмитаж2"),
+	Place(-47.342052, -68.559948, "Эрмитаж3"),
+	Place(20.206243, -75.167841, "Эрмитаж4"),
+	Place(48.900373, -73.653973, "Эрмитаж5")
+)
+
+fun bearing(from: Place, to: Place): Double {
+	val lat1 = from.latitude / 180 * PI
+	val lng1 = from.longitude / 180 * PI
+	val lat2 = to.latitude / 180 * PI
+	val lng2 = to.longitude / 180 * PI
+
+	val x = cos(lat2) * sin(lng2 - lng1)
+	val y = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(lng2 - lng1)
+
+	return ((atan2(x, y) * 180 / PI) + 360) % 360
+}
+
